0c42b9d10e33dbb03b7c6d8f4f822af19df379a6
Marek Vavrusa <marek@vavrusa.com>
2010-01-20 22:37:54 +0100

    Server stub.

---
+++ src/clientserver/socket.cpp
@@ -0,0 +1,134 @@
+/***************************************************************************
+ *   Copyright (C) 2010 Marek Vavrusa <xvavru00@stud.fit.vutbr.cz          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "socket.hpp"
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <netdb.h>
+
+using namespace std;
+
+Socket::Socket(int fd)
+   : mSock(fd), mPort(DefaultPort)
+{
+}
+
+Socket::~Socket()
+{
+}
+
+int Socket::create()
+{
+   // Create socket
+   mSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+   // Reuse open socket
+   int state = 1;
+   if(setsockopt(mSock, SOL_SOCKET, SO_REUSEADDR, (const char*) &state, sizeof(state)) < 0)
+      return -1;
+
+   return 0;
+}
+
+int Socket::connect(const std::string& host, int port)
+{
+   // Ignore multiple connect
+   if(isOpen())
+      return false;
+
+   // Create socket
+   hostent* hent = 0;
+   if((hent = gethostbyname(host.c_str())) == 0)
+      return BadAddr;
+
+   if(create() < 0)
+      return IOError;
+
+   // Connect to host
+   bzero(&mAddr, sizeof(mAddr));
+   mAddr.sin_family = AF_INET;
+   mAddr.sin_port = htons(port);
+   memcpy(&mAddr.sin_addr, hent->h_addr, hent->h_length);
+   if(::connect(mSock, (sockaddr*) &mAddr, sizeof(mAddr)) < 0) {
+      return ConnectError;
+   }
+
+   // Done
+   mHost = host;
+   mPort = port;
+   return Ok;
+}
+
+int Socket::send(const char* buf, size_t size)
+{
+   if(!isOpen())
+      return NotOpen;
+
+   int sent = 0;
+   if((sent = ::send(mSock, buf, size, 0)) < 0)
+      return SendError;
+
+   return sent;
+}
+
+int Socket::listen(int port, int limit)
+{
+   // Create socket
+   if(create() < 0)
+      return IOError;
+
+   // Bind to given port
+   if(bind(port) < 0)
+      return IOError;
+
+   // Listen
+   if(::listen(mSock, limit) < 0)
+      return IOError;
+
+   return Ok;
+}
+
+int Socket::bind(int port)
+{
+   // Create address
+   mAddr.sin_family = AF_INET; // Addr type
+   mAddr.sin_port = htons(port); // Set port
+   mAddr.sin_addr.s_addr = INADDR_ANY; // First available addr
+
+   if(::bind(mSock, (sockaddr*) &mAddr, sizeof(mAddr)) < 0)
+      return -1;
+
+   mPort = port;
+   return 0;
+}
+
+
+int Socket::close()
+{
+   // Close open socket
+   if(isOpen()) {
+      if(::close(mSock) < 0)
+         return IOError;
+
+      mSock = -1;
+   }
+
+   return Ok;
+}
---
+++ src/clientserver/socket.hpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *   Copyright (C) 2010 Marek Vavrusa <xvavru00@stud.fit.vutbr.cz          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#pragma once
+#ifndef __socket_hpp__
+#define __socket_hpp__
+#include <string>
+#include <netinet/in.h>
+
+/** C++ style wrapper for BSD sockets with state checking and error control.
+  */
+class Socket
+{
+   public:
+      Socket(int fd = -1);
+      virtual ~Socket();
+
+      // Connect to remote host:port
+      int connect(const std::string& host = "localhost", int port = 0);
+
+      // Listen on given port
+      int listen(int port, int limit = 5);
+
+      // Close connection
+      int close();
+
+      // Send raw data
+      int send(const char* buf, size_t size);
+
+      // Receive raw data
+      int recv(char* dst, int size, int flags = 0) {
+         return ::recv(mSock, dst, size, flags);
+      }
+
+      // Returns whether is socket associated
+      bool isOpen() { return mSock != -1; }
+
+      // Returns socket id
+      int sock() { return mSock; }
+
+      // Returns port
+      int port() { return mPort; }
+
+      // Returns host as std::string reference
+      const std::string& host() { return mHost; }
+
+      // Return address as struct
+      sockaddr_in& addr() { return mAddr; }
+
+      // Error enumeration
+      enum {
+         IOError        = -128,
+         BadAddr,
+         ConnectError,
+         CloseError,
+         SendError,
+         RecvError,
+         NotOpen,
+         Ok             = 1,
+         DefaultPort    = 0
+      };
+
+   protected:
+      // Create TCP sockets
+      int create();
+
+      // Bind to port
+      int bind(int port);
+
+   private:
+      int mSock;
+      int mPort;
+      sockaddr_in mAddr;
+      std::string mHost;
+};
+
+#endif // __socket_hpp__
