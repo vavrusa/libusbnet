8b0183465529cb2d0a6576a5b836e0923cfaa421
Marek Vavrusa <marek@vavrusa.com>
2010-01-28 00:39:33 +0100

    Moved macros and types to common header.

--- src/common.h
+++ src/common.h
@@ -16,45 +16,41 @@
 *   Free Software Foundation, Inc.,                                       *
 *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
 ***************************************************************************/
-
+#ifndef __common_h__
+#define __common_h__
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "usbnet.h"
-#include "common.h"
+#include <dlfcn.h>
 
-void usb_init(void)
-{
-   static void (*func)(void) = NULL;
-   LOAD_NEXT_SYM(func, "usb_init");
-   NOT_IMPLEMENTED;
+/* Helper function to return only base name from path. */
+static const char* filename(const char* path) {
+   const char* p = strrchr(path, '/');
+
+   if(p == NULL)
+      return path;
+
+   return ++p;
 }
 
-int usb_find_busses(void)
-{
-   static int (*func)(void) = NULL;
-   READ_SYM(func, "usb_find_busses");
-   NOT_IMPLEMENTED;
+/** Debug macro for marking unimplemented calls. */
+#ifdef DEBUG
+#define NOT_IMPLEMENTED \
+   fprintf(stderr, "[!!] %s:%d: function '%s' not implemented\n", filename(__FILE__), __LINE__, __func__);
+#else
+#define NOT_IMPLEMENTED
+#endif
 
-   return 0;
+/** Load next symbol from linked shared libraries.
+  * Return error if not found and exit with error state (1).
+  */
+#define READ_SYM(sym, ident) { \
+   if((sym) == NULL) { \
+      (sym) = dlsym(RTLD_NEXT, (ident)); \
+      char* __res = dlerror(); \
+      if (__res != NULL) { \
+         fprintf(stderr, "[!!] %s:%d: READ_SYM(%s): %s\n", filename(__FILE__), __LINE__, (ident), __res); \
+         exit(1); \
+      } \
+   } \
 }
 
-int usb_find_devices(void)
-{
-   static int (*func)(void) = NULL;
-   READ_SYM(func, "usb_find_devices");
-   NOT_IMPLEMENTED;
-
-   return 0;
-}
-
-struct usb_bus* usb_get_busses(void)
-{
-   static struct usb_bus* (*func)(void) = NULL;
-   READ_SYM(func, "usb_get_busses");
-   NOT_IMPLEMENTED;
-
-   return NULL;
-}
-
+#endif // __common_h__
