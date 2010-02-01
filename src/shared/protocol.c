f8667c11a0f2514782fd8340f947ee2787036edf
Marek Vavrusa <marek@vavrusa.com>
2010-02-01 01:27:27 +0100

    Updated protocol, created core API.

--- src/shared/protocol.c
+++ src/shared/protocol.c
@@ -18,10 +18,119 @@
 ***************************************************************************/
 
 #include "protocol.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-char* pkt_init(char* buf, Type t)
+packet_t* pkt_new(int size) {
+
+   // Minimum packet size is PACKET_MINSIZE
+   if(size < PACKET_MINSIZE) {
+      fprintf(stderr, "%s: minimum packet size is %dB\n", __func__, PACKET_MINSIZE);
+      size = PACKET_MINSIZE;
+   }
+
+   packet_t* pkt = malloc(sizeof(packet_t));
+   pkt->buf = malloc(size * sizeof(char));
+   pkt->size = size;
+   return pkt;
+}
+
+void pkt_del(packet_t* pkt) {
+   free(pkt->buf);
+   free(pkt);
+}
+
+void pkt_init(packet_t* pkt, Type op)
 {
-   buf[0] = (char) t;
-   buf[1] = (char) 0;
-   return buf + 1;
+   // Clear
+   memset(pkt->buf, 0, pkt->size);
+
+   // Set opcode
+   pkt->buf[0] = (char) op;
+
+   // Set data pos
+   pkt->pos = PACKET_MINSIZE;
+}
+
+int pkt_size(packet_t* pkt)
+{
+   return pkt->pos;
+}
+
+int pkt_send(int fd, packet_t* pkt)
+{
+   // Send buffer
+   int res = send(fd, pkt->buf, pkt_size(pkt), 0);
+   printf("%s(%d, size %d) = %d\n", __func__, fd, pkt_size(pkt), res);
+   return res;
+}
+
+int pkt_recv(int fd, packet_t* dst)
+{
+   // Prepare
+   char* p = dst->buf;
+   int rcvd = 0; uint16_t pending = PACKET_MINSIZE;
+
+   // Read packet header
+   while(pending != 0) {
+      rcvd = recv(fd, p, pending, 0);
+      printf("Read: %d pending %d (val 0x%x).\n", rcvd, pending, *p);
+      if(rcvd <= 0) {
+         return -1;
+      }
+      pending -= rcvd;
+      p += rcvd;
+   }
+
+   // Read packet payload
+   dst->pos = PACKET_MINSIZE;
+   pending = (uint16_t) *(dst->buf + 1);
+   printf("Packet: 0x%x 0x%x 0x%x (expected size %dB)\n",
+          dst->buf[0], dst->buf[1], dst->buf[2], pending);
+
+   while(pending != 0) {
+      rcvd = recv(fd, p, pending, 0);
+      if(rcvd <= 0)
+         return -1;
+      printf("Read: %d pending %d.\n", rcvd, pending);
+      pending -= rcvd;
+      p += rcvd;
+   }
+
+   // Handle incoming packet
+   printf("Packet: loaded payload.\n");
+   dst->pos += (uint16_t) *(dst->buf + 1);
+   return dst->pos;
+}
+
+int pkt_append(packet_t* pkt, Type type, uint16_t len, void* val)
+{
+   char* dst = pkt->buf + pkt->pos;
+
+   // Write T-L-V
+   uint16_t written = 0;
+   memcpy(dst + written, &type, sizeof(char));    written += sizeof(char);
+   memcpy(dst + written, &len, sizeof(uint16_t)); written += sizeof(uint16_t);
+   memcpy(dst + written, val, len);               written += len;
+   printf("Packet: appended type: 0x%x len: %d total: %d\n", type, len, written);
+
+   // Update write pos and packet size
+   pkt->pos += written;
+   uint16_t nsize = pkt->pos - PACKET_MINSIZE;
+   memcpy(pkt->buf + 1, &nsize, sizeof(uint16_t));
+   return written;
+}
+
+void pkt_begin(packet_t* pkt, param_t* param)
+{
+   if(pkt_size(pkt) > PACKET_MINSIZE)
+      *param = pkt->buf + PACKET_MINSIZE;
+   else
+      *param = pkt_end(pkt);
+}
+
+void* pkt_end(packet_t* pkt)
+{
+   return pkt->buf + pkt->pos;
 }
--- src/shared/protocol.h
+++ src/shared/protocol.h
@@ -20,6 +20,7 @@
 #pragma once
 #ifndef __protocol_h__
 #define __protocol_h__
+#include <stdint.h>
 
 /** ASN.1/BER semantic types
   */
@@ -38,6 +39,10 @@ typedef enum {
    // Calls
    NullRequest    = CallType,       // Ping
    UsbInit        = CallType  +  1, // usb_init()
+   UsbFindBusses  = CallType  +  2, // usb_find_busses()
+   UsbFindDevices = CallType  +  3, // usb_find_devices()
+   UsbGetBusses   = CallType  +  4, // usb_bus* usb_get_busses()
+
 } Type;
 
 /* Packet manipulation. */
@@ -46,7 +51,103 @@ extern "C"
 {
 #endif
 
-char* pkt_init(char* buf, Type t);
+/** Packet structure. */
+typedef struct {
+   unsigned size;
+   unsigned pos;
+   char* buf;
+} packet_t;
+
+/** Parameter structure. */
+typedef char* param_t;
+
+#define PACKET_MINSIZE 3 // 1B op + 2B size
+#define PARAM_TLEN     sizeof(char)
+#define PARAM_LLEN     sizeof(uint16_t)
+#define PARAM_LEN      PARAM_TLEN+PARAM_LLEN
+
+/** Initialize packet on existing buffer.
+  * \param buf must be char* or char[]
+  * \param size real buffer size
+  */
+#define pkt_create(buf, size) { size, 0, buf }
+
+/** Allocate new packet.
+  * Minimal packet size is 1 + 4B.
+  * \param size required packet size
+  * \return new packet
+  */
+packet_t* pkt_new(int size);
+
+/** Free allocated packet.
+  * Use only with dynamically allocated packets.
+  * \param pkt freed packet
+  */
+void pkt_del(packet_t* pkt);
+
+/** Initialize packet.
+  * \param pkt initialized packet
+  * \param op packet opcode
+  */
+void pkt_init(packet_t* pkt, Type op);
+
+/** Return packet size.
+  * \param pkt packet
+  */
+int pkt_size(packet_t* pkt);
+
+/** Append parameter to packet.
+  * \param pkt packet
+  * \param type parameter type
+  * \param len  parameter size
+  * \param val  parameter value
+  * \return bytes written
+  */
+int pkt_append(packet_t* pkt, Type type, uint16_t, void* val);
+
+/** Send packet.
+  * \param fd destination filedescriptor
+  * \param pkt packet
+  * \return send() value
+  */
+int pkt_send(int fd, packet_t* pkt);
+
+/** Receive packet.
+  * \param fd source fd
+  * \param pkt destination packet
+  * \return recv() return value
+  */
+int pkt_recv(int fd, packet_t* dst);
+
+/** Return packet first symbol.
+  * \param pkt source packet
+  * \param param target symbol
+  */
+void pkt_begin(packet_t* pkt, param_t* param);
+
+/** Return packet end.
+  * Param length + 1.
+  */
+void* pkt_end(packet_t* pkt);
+
+/** Return parameter type.
+  * \return parameter type
+  */
+#define param_type(param) (char)*(param)
+
+/** Return parameter length.
+  * \return parameter length
+  */
+#define param_len(param) (uint16_t)*((param) + PARAM_TLEN)
+
+/** Return parameter value.
+  * \return parameter value
+  */
+#define param_val(param) ((param) + PARAM_LEN)
+
+/** Next parameter.
+  */
+#define param_next(param) ((param) + PARAM_LEN + param_len((param)))
 
 #ifdef __cplusplus
 }
